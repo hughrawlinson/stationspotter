aa4d30ecf5099019405f9a83609b22e19f2268ff
Hugh Rawlinson <hugh@codeoclock.net>
2013-04-21 05:39:02 +0100

    I don't know, bug fix or something

--- nodebackend.js
+++ nodebackend.js
@@ -1,20 +1,13 @@
 var Pusher = require( 'pusher' );
 var http = require('http');
+var url = require('url');
 
 var pusher = new Pusher({ appId: "38442", key: "1ccd6fd9880863b97f0d", secret: "dd9e528ed20dca277c00" });
 
 http.createServer(function (req, result) {
-	if (req.method == 'POST') {
-        var body = '';
-        req.on('data', function (data) {
-            body += data;
-        });
-        req.on('end', function () {
-
-            var POST = qs.parse(body);
-            console.log(POST);
-            pusher.trigger( "space_apps", "sighting", POST );
-
-        });
-    }
+	console.log('1');
+	var url_parts = url.parse(request.url, true);
+	var query = url_parts.query;
+	console.log(query)
+	pusher.trigger( "space_apps", "sighting", query );
 }).listen(3000);
